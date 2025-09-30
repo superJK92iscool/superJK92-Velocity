d42bb740992d897dec57fbb7a2e3ff763dbc0cc1
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2025-09-29 19:54:19 -0500

    Update MinecraftChannelIdentifierTest.java 2

--- api/src/test/java/com/velocitypowered/api/proxy/messages/MinecraftChannelIdentifierTest.java
+++ api/src/test/java/com/velocitypowered/api/proxy/messages/MinecraftChannelIdentifierTest.java
@@ -44,7 +44,7 @@ class MinecraftChannelIdentifierTest {
 
   @Test
   void createAllowsSlashes() {
-    create("velocity", "test/test2");
+    create("superjk92-velocity", "test/test2");
   }
 
   @Test
