bf8345efc6274c82e794a87ff9e0008af19a365f
superJK92iscool <50285310+superJK92iscool@users.noreply.github.com>
2025-09-29 19:52:53 -0500

    Update build.gradle.kts

--- proxy/build.gradle.kts
+++ proxy/build.gradle.kts
@@ -20,7 +20,7 @@ tasks {
 
     jar {
         manifest {
-            attributes["Implementation-Title"] = "Velocity"
+            attributes["Implementation-Title"] = "superJK92-Velocity"
             attributes["Implementation-Vendor"] = "Velocity Contributors"
             attributes["Multi-Release"] = "true"
         }
